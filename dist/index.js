0583f6356b277d256b15060c90f83b9e507adc5c
ZixiaoWang <www.wangzixiao@gmail.com>
2021-03-02 12:20:37 +0800

    Init projects

---
+++ dist/index.js
@@ -0,0 +1,95 @@
+function showDocByCmd(cmd = '') {
+    if (cmds.includes(cmd.toLocaleLowerCase()) === false) {
+        return void 0;
+    }
+
+}
+function getCandidates(keywordString = '') {
+    if (keywordString.length < 3) {
+        return [];
+    }
+
+    const keywords = keywordString.toLocaleLowerCase().split(' ');
+    return cmds
+        .map((cmd) => {
+            const matchedKeywords = [];
+            for (let keyword of keywords) {
+                if (cmd.includes(keyword)) {
+                    matchedKeywords.push(keyword);
+                }
+            }
+
+            if (matchedKeywords.length === 0) {
+                return false;
+            } else {
+                return {
+                    cmd,
+                    matchedKeywords
+                }
+            }
+        })
+        .filter(item => !!item)
+}
+
+function displayListByCandidates(candidates) {
+    const list = document.getElementById('candidates');
+    if (list) {
+        let bullets = '';
+        candidates.forEach((candidate) => {
+            const reg = new RegExp(`(${candidate.matchedKeywords.join('|')})`, 'g');
+            const innerContent = candidate.cmd.replace(reg, `<b data-id="${candidate.cmd}">$1</b>`);
+            bullets += `<li data-id="${candidate.cmd}">tpm2 ${innerContent}</li>`;
+        });
+
+        if (bullets.length > 0) {
+            fillElement(list, bullets);
+        }
+    }
+}
+
+function resetElement(element) {
+    if (element) {
+        element.classList.add('hidden');
+        element.innerHTML = '';
+    }
+}
+
+function fillElement(element, innerHTML) {
+    if (element) {
+        element.classList.remove('hidden');
+        element.innerHTML = innerHTML;
+    }
+}
+
+
+
+window.addEventListener('load', () => {
+    const input = document.getElementById('input');
+    const list = document.getElementById('candidates');
+    const docContainer = document.getElementById('doccontainer');
+
+
+    if (input && list && docContainer) {
+        input.addEventListener('keyup', (event) => {
+            const keywords = event.target.value || '';
+            const candidates = getCandidates(keywords);
+            resetElement(docContainer);
+
+            if (candidates.length > 0) {
+                displayListByCandidates(candidates);
+            } else {
+                resetElement(list);
+            }
+        });
+
+        list.addEventListener('click', (event) => {
+            const id = event.target.dataset.id;
+            const template = document.getElementById(id);
+            input.value = 'tpm2 ' + id;
+            resetElement(list);
+            if (template) {
+                fillElement(docContainer, template.innerHTML);
+            }
+        })
+    }
+})
